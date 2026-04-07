22bbbd9022249896848bacbc91daa0366b8efc81
Yuval Taubes <yuval.taubes@gmail.com>
2026-04-07 10:43:12 -0500

    first commit

---
+++ PriorityQueue.java
@@ -0,0 +1,18 @@
+public class PriorityQueue<T>
+{
+    public PriorityQueue()
+    {
+    }
+
+    public void insert(int priority, T data)
+    {
+    }
+
+    public T retrieveMax()
+    {
+    }
+
+    public void print()
+    {
+    }
+}
---
+++ Tree.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+public class Tree<E>
+{
+    public Tree()
+    {
+    }
+
+    public void insert(int key, E value)
+    {
+    }
+
+    public E search(int key)
+    {
+    }
+
+    public void print()
+    {
+    }
+}
